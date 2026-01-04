2b4f1ebad9ad910de54fc809624619e340c8767f
Juan I. <juanpalaciagar@gmail.com>
2026-01-04 17:08:41 +0100

    feat: context implementation finished

---
+++ src/components/QuizControl/QuizControl.jsx
@@ -0,0 +1,14 @@
+import QuizLanding from "./../QuizLanding/QuizLanding"
+import QuizComponent from "./../QuizComponent/QuizComponent"
+import { useContext } from "react"
+import { QuizContext } from "./../../store/quiz-context"
+
+export default function QuizControl() {
+        const { quizStatus } = useContext(QuizContext)
+        const quizLength = quizStatus.length
+    return (
+        <main>
+            {quizLength ? <QuizComponent quiz={quizStatus} /> : <QuizLanding />}
+        </main>
+    )
+}
